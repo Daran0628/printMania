a7ce5c22f985bb9a31f23feb30882554edcc607d
daran <moon.daran.s@gmail.com>
2024-11-11 16:21:11 +0900

    24-11-11 class

--- js/main.js
+++ js/main.js
@@ -3,7 +3,22 @@
 // *** header
 // * * * nav
 
-$(".gnb>li").hover(function () {
-  console.log("it works");
-  $(this).children(".depth_2").slideDown();
+$(".gnb>li").hover(
+  function () {
+    $(this).children("ul").css("display", "flex");
+  },
+  function () {
+    $(this).children("ul").hide();
+  }
+);
+// * * * user_util
+// // *** search
+
+$(".search").click(function () {
+  $(".search_modal").addClass("show");
+  $(".search.open").hide();
+});
+$(".search_close").click(function () {
+  $(".search_modal").removeClass("show");
+  $(".search.open").show();
 });
